b8adb3a2935bdd71ac394cd95b85642713e439bc
HiroUrata <1712026@i.softbank.jp>
2021-12-03 22:12:27 +0900

    ViewController search機能　追加

--- APILessonNormalApp/Controller/ViewController.swift
+++ APILessonNormalApp/Controller/ViewController.swift
@@ -14,14 +14,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var resultTableView: UITableView!
     
+    private let alamofireProcess = AlamofireProcess()
+    private let dispatchGroup = DispatchGroup()
+    private let dispatchQueue = DispatchQueue(label: "Process",attributes: .concurrent)
+
+    private var cellContentsArray = [ItemDetailDatas]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
+//        resultTableView.delegate = self
+//        resultTableView.dataSource = self
         
     }
 
     @IBAction func search(_ sender: UIButton) {
+        
+        dispatchGroup.enter()
+        dispatchQueue.async {[self] in
+            
+            alamofireProcess.getItemDetailData(searchKey: searchTextField.text) { result, error in
+                
+                if error != nil{
+                    
+                    return
+                }
+                
+                self.cellContentsArray = result!
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            
+            self.resultTableView.reloadData()
+        }
     }
     
 }
+
 
